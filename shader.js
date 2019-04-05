f84b4e56645f7ad87f740a602bbc07c887546882
xiaofanlin <xiaofanlinus@gmail.com>
2019-04-05 06:54:14 -0700

    working on shader loader

---
+++ shader.js
@@ -0,0 +1,29 @@
+
+
+class Shader {
+    /**
+     * @param {WebGLRenderingContext} gl
+     */
+    constructor(gl) {
+        // shader program reference
+        const mShaderProgram = gl.createProgram()
+        let mVertSrc, mFragSrc;
+    }
+
+    /**
+     * @param  {string} filename
+     */
+    readSrc(filename) {
+
+    }
+
+    link() {
+
+    }
+
+    compile() {
+
+    }
+
+    
+}
